9540d37c5bc8f339c7c301172471bc27c31f0aca
Denis Kostyuhin <den.d.xps@gmail.com>
2024-08-29 15:23:27 +0700

    for review

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Den-d-xps/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardConteiner = document.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
+function createCard (data, callback) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const delButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = data.link;
+  cardElement.querySelector('.card__title').textContent = data.name;
+
+  delButton.addEventListener('click', callback);
+
+  return cardElement
+};
+
 
 // @todo: Функция удаления карточки
+function deleteCard (evt) {
+  const cardItem = evt.target.closest('.card');
+  cardItem.remove();
+};
+
 
 // @todo: Вывести карточки на страницу
+const cardsListElement = initialCards.map((item) => createCard(item, deleteCard));
+ 
+cardsListElement.forEach(element => {
+  cardConteiner.append(element);
+});
